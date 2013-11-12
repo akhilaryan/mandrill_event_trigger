0abaf376e95ee950bc2870fa9c2957c3caba2300
John Varghese <jvarghese@Administrators-MacBook-Pro.local>
2013-11-11 20:02:04 -0800

    Created the rails project framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MandrillEventTrigger::Application.config.secret_key_base = '816c93017d76098041fea9bf32d0e0af0d2530a910686febb2f1fbfe712e84808a817833ba3a9a8ec3935718475d5dab74539d8ac613ece3cb379e9584c22fd0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MandrillEventTrigger::Application.config.session_store :cookie_store, key: '_mandrill_event_trigger_session'
